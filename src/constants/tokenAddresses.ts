db4e18dc1a4f6e3ea1607afb83a3679498e5f122
Julian Yap <julianokyap@gmail.com>
2020-12-21 22:34:31 -1000

    Set Yield Farm address. Remove some unused addresses

--- src/constants/tokenAddresses.ts
+++ src/constants/tokenAddresses.ts
@@ -7,16 +7,8 @@ export const yamv3 = "0x0AaCfbeC6a24756c20D41914F2caba817C0d8521";
 export const yUsd = "0x5dbcf33d8c2e976c6b560249878e6f1491bca25c";
 export const DPI = "0x1494CA1F11D487c2bBe4543E90080AeBa4BA3C2b";
 export const INDEX = "0x0954906da0Bf32d5479e25f46056d22f08464cab";
-export const reservesContractv2 = "0xCF27cA116dd5C7b4201c75B46489D1c075362087";
 export const ESCHUBQSLPAddress = "0xF102Ad140b26C3c6AF9E9358DA9dEAA27CB1dbEA";
 
-export const OldContractIncentivizer = "0x5b0501F7041120d36Bc8c6DC3FAeA0b74b32a0Ed";
-export const ContractGovernor = "0xc32f9b0292965c5dd4a0ea1abfcc1f5a36d66986";
 export const ContractTimelock = "0x8b4f1616751117C38a0f84F9A146cca191ea3EC5";
-export const ContractRebaser = "0xd93f403b432d39aa0f736c2021be6051d85a1d55";
-export const ContractIncentivizer = "0xD67c05523D8ec1c60760Fd017Ef006b9F6e496D0";
-export const ContractReserves = "0x97990b693835da58a281636296d2bf02787dea17";
-export const ContractContributorGovernor = "0xdcec4a3aa84f79249c1b5325a06c1560d202dd87";
-export const ContractContribtorTimelock = "0xd40a03e520d49339e91bc58c2c9b8966ee7f490f";
+export const ContractIncentivizer = "0x8F8c66370f05DB40ea1f7F5a2064c588920e8599";
 export const ContractIndexStaking = "0x205Cc7463267861002b27021C7108Bc230603d0F";
-export const ContractMonthlyAllowance = "0x03A882495Bc616D3a1508211312765904Fb062d1";
